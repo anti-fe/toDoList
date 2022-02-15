7b59a0154cf608ef7023012bd4cfc78ac01c9a37
anti <raylyangosha@gmail.com>
2022-02-15 17:15:09 +0300

    add, del and check items

--- js/app.js
+++ js/app.js
@@ -1,25 +1,87 @@
 const userInp = document.querySelector('.header-input'),
+        todoCont = document.querySelector('.todo-container'),
         btnAdd = document.querySelector('.header-button'),
-        todoList = document.querySelector('.todo-list');
+        todoList = document.querySelector('.todo-list'),
+        btnsRemove = document.querySelectorAll('.todo-remove');
 
 
 btnAdd.addEventListener('click', addItem);
+todoCont.addEventListener('click', actionItem);
+
+let todoData = {
+    'completed': [],
+    'notCompleted': [],
+}
+
+// (function setLocalStorage(){
+//     if(localStorage.getItem('todoStates') {
+//         todoData
+//     }
+// }())
 
 let inpValue;
 
+function createItem() {
+    let todoItem = document.createElement('li');
+    todoItem.classList.add('todo-item');
+
+    let todoText = document.createElement('span');
+    todoText.textContent = inpValue;
+    todoText.classList.add('text-todo');
+
+    let todoBtns = document.createElement('div');
+    todoBtns.classList.add('todo-buttons');
+
+    let todoRemove = document.createElement('button');
+    todoRemove.classList.add('todo-remove');
+        
+    let todoComplete = document.createElement('button');
+    todoComplete.classList.add('todo-complete');
+
+    todoBtns.appendChild(todoRemove);
+    todoBtns.appendChild(todoComplete);
+    todoItem.appendChild(todoText);
+    todoItem.appendChild(todoBtns);
+    todoList.appendChild(todoItem);
+}
+
 function addItem(e) {
     e.preventDefault();
-    inpValue = userInp.value;
+    inpValue = userInp.value.trim();
 
-    if((inpValue) || (inpValue != '')) {
+    if(inpValue) {
         userInp.style.border = "none";
-        let todoItem = document.querySelector('.todo-item').cloneNode(true);
-        todoList.appendChild(todoItem);
-        todoList.lastChild.querySelector('.text-todo').textContent = inpValue;
-        console.log(todoList.lastChild);
+
+        createItem();
+        userInp.value = '';  
     } else {
         userInp.style.border = "2px solid rgba(255, 0, 0, 0.575)";
     }
-
 }
 
+
+function actionItem(e) {
+    e.preventDefault();
+    let target = e.target;
+    //DELETE
+    if((target.tagName == 'BUTTON') && (target.classList.contains("todo-remove"))) {
+        let parentTarget = target.parentNode.parentNode;
+        parentTarget.parentNode.removeChild(parentTarget);
+    };
+    //CHECK
+    if((target.tagName == 'BUTTON') && (target.classList.contains("todo-complete"))) {
+        let parentTarget = target.parentNode.parentNode.parentNode;
+        let itemTarget = parentTarget.querySelector('.todo-item');
+        console.log(parentTarget);
+        if(parentTarget.classList.contains('todo-completed')) {
+            parentTarget.parentNode.querySelector('.todo-list').appendChild(itemTarget);
+        } else {
+            parentTarget.parentNode.querySelector('.todo-completed').appendChild(itemTarget);
+        }
+    };
+}
+
+function setLocalStorage(){
+    todoData['notCompleted'].push(todoText.textContent);
+    localStorage.setItem('todoStates', JSON.stringify(todoData));
+}
